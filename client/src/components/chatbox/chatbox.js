0f417a2163ecf104a88687fbb7f741b42dbbd538
Ashutosh <rajputashu7200@gmail.com>
2023-05-04 19:28:15 +0530

    Chat display by Dev

--- client/src/components/chatbox/chatbox.js
+++ client/src/components/chatbox/chatbox.js
@@ -1,77 +1,101 @@
-import React from 'react'
-import {useEffect,useState} from 'react'
-import{Link,useParams} from "react-router-dom"
-import { read_cookie,delete_cookie } from 'sfcookies'
-import axios from 'axios'
-const URL="http://localhost:3001/DoctorMain/history"
-const URL1= "http://localhost:3000/newchat/";
-const URL2="http://localhost:3000/newmessage/"
-const Chatbox = () => {
-    const docemail=read_cookie('dev');
-    const [email,setemail]=useState("")
-    const [currentmsg,setcurrentmsg]=useState(null);
-    const id=useParams()
-    const patientemail=id.id;
-    console.log(patientemail);
-    const fetchdetails=()=>{
-      fetch(`http://localhost:3001/DoctorMain/history/${id}`)
-      .then(res=>res.json())
-        .then(data=>setemail(data))
-      }
-      useEffect(()=>{
-        fetchdetails();
-      },[email]);
-   const doctormail= read_cookie("dev");
-  
-  const [object,Setobject]= useState([]);
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+import axios from 'axios';
+import './chatbox.css';
+import { format } from 'timeago.js';
 
-  const [currentmessage,Setcurrentmessage]=useState(null);
-  // this is branch
-  useEffect(()=>{
-    const getconversation= async ()=>{
-      try{
-        const res= await axios.get(URL1+doctormail);
-        //console.log(res);
+const URL = 'http://localhost:3001/DoctorMain/history';
+const URL1 = 'http://localhost:3000/newchat/';
+const URL2 = 'http://localhost:3000/newmessage/';
+
+const Chatbox = () => {
+  const docemail = read_cookie('dev');
+  const [email, setemail] = useState('');
+  const [currentmsg, setcurrentmsg] = useState(null);
+  const { id } = useParams();
+  const patientemail = id;
+  console.log(patientemail);
+
+  const fetchdetails = () => {
+    fetch(`http://localhost:3001/DoctorMain/history/${id}`)
+      .then((res) => res.json())
+      .then((data) => setemail(data));
+  };
+
+  useEffect(() => {
+    fetchdetails();
+  }, [email]);
+
+  const doctormail = read_cookie('dev');
+  console.log(doctormail);
+
+  const [object, Setobject] = useState([]);
+
+  const [currentmessage, Setcurrentmessage] = useState([{}]);
+
+  useEffect(() => {
+    const getconversation = async () => {
+      try {
+        const res = await axios.get(URL1 + doctormail);
         Setobject(res.data);
-       console.log(res)
-      }catch(err){
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }{}
+    };
+    getconversation();
+  }, [doctormail]);
+
+  var conversationId = null;
+  for (var j = 0; j < object.length; j++) {
+    const arr = object[j].members;
+    if (arr[0] === docemail && arr[1] === patientemail) {
+      conversationId = object[j]._id;
+    }
+  }
+  console.log(conversationId);
+
+  useEffect(() => {
+    const getmessages = async () => {
+      try {
+        const res = await axios.get(URL2 + conversationId);
+        console.log(res);
+        Setcurrentmessage(res.data);
+      } catch (err) {
         console.log(err);
       }
+    };
+    if (conversationId) {
+      getmessages();
     }
-    getconversation();
-  },[]);
+  }, [conversationId]);
 
-        //searching in object
-        var conversationId=null 
-        for(var j=0;j<object.length;j++){
-          const arr=object[j].members;
-          if(arr[0]===docemail && arr[1]===patientemail){
-            conversationId=object[j]._id;
-          }
-        }
-      // ye message fetch ke liye hh conversationid ke basis pe
-   useEffect(()=>{
-     const getmessages=async ()=>{
-        try{
-          const res= await axios.get(URL2+conversationId);
-          console.log(res)
-        }catch(err){
-          console.log(err);
-        }
-     }
-     getmessages();
-   })
+  // console.log(currentmessage[0]);
 
-
-
-
-
-
+  // const own = currentmessage[0];
+  // console.log(own);
+  
   return (
-    <div>
-      helo
+    <>
+    {currentmessage.map((own)=>(
+      <div className={own.sender ? 'message own' : 'message'}>
+      <div className="messageTop">
+        <img className="messageImg" alt="pics" />
+        <p className="messageText">{own.text}</p>
+      </div>
+      <div className="messageBottom">{format(own.createdAt)}</div>
     </div>
-  )
-}
+    ))}
+    {/* <div className={own.sender ? 'message own' : 'message'}>
+      <div className="messageTop">
+        <img className="messageImg" alt="pics" />
+        <p className="messageText">{own.text}</p>
+      </div>
+      <div className="messageBottom">{format(own.createdAt)}</div>
+    </div> */}
+    </>
+  );
+};
 
-export default Chatbox
+export default Chatbox;
