b202dc3ce7f48af6ff19d7950fb412e04801ed9e
Ashutosh <rajputashu7200@gmail.com>
2023-06-12 13:36:26 +0530

    patient central page done Ashutosh

--- client/src/components/Patient/centralpagePatient.js
+++ client/src/components/Patient/centralpagePatient.js
@@ -1,90 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import PatientNavbar from './PatientNavbar'
 import './centralpagePatient.css'
 import { Link } from 'react-router-dom'
 import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import Instagram from "../../img/instagram.png";
-import Vector1 from "../../img/hospital.jpg";
-// import Vector2 from "../../img/Vector2.png";
 function Patient() {
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <div>
-      <PatientNavbar/>
-      <div className="Intro" id="Intro">
-      {/* left name side */}
-      <div className="i-left">
-        <div className="i-name">
-          {/* yahan change hy darkmode ka */}
-          <span>Welcome To</span>
-          <span>Unified Health Interface</span>
-          <span>
-            Frontend Developer with high level of experience in web designing
-            and development, producting the Quality work
-          </span>
+    <div className='patientmaindiv'>
+      <div className='patientmainimg'>
+        {/* <h1>Image div</h1> */}
+      </div>
+
+      <div className='patientloginclass'>
+        <div className="patienttextdiv">
+
+          <p>Welcome To UHI</p>
+
         </div>
-        <Link to="/patient/login" smooth={true} spy={true}>
-          <button className="button-i-button">Login</button>
-        </Link>
-        {/* social icons */}
-        <div className="i-icons">
-          <img src={Github} alt="" />
-          <img src={LinkedIn} alt="" />
-          <img src={Instagram} alt="" />
+        <div className="patientbuttondiv">
+          <button className="patientbuttonclass">
+            <NavLink
+              exact
+              to="/Patient/login"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Login
+            </NavLink>
+          </button>
+          <button className="patientbuttonclass">
+            <NavLink
+              exact
+              to="/Patient/Signup"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Register
+            </NavLink>
+          </button>
         </div>
       </div>
-      {/* right image side */}
-      <div className="i-right">
-        <div className='img'>
-        <img src={Vector1} alt="" />
-        </div>
-        {/* <img src={Vector2} alt="" /> */}
-        {/* <img src={medi} alt="" /> */}
-        {/* animation */}
-        {/* <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
-          transition={transition}
-          src={glassesimoji}
-          alt=""
-        /> */}
+    </div >
 
-        {/* <motion.div
-          initial={{ top: "-4%", left: "74%" }}
-          whileInView={{ left: "68%" }}
-          transition={transition}
-          className="floating-div"
-        >
-          <button><FloatinDiv img={crown} text1="Web" text2="Developer" /></button>
-        </motion.div> */}
 
-        {/* animation */}
-        {/* <motion.div
-          initial={{ left: "9rem", top: "18rem" }}
-          whileInView={{ left: "0rem" }}
-          transition={transition}
-          className="floating-div"
-        > */}
-          {/* floatinDiv mein change hy dark mode ka */}
-          {/* <button><FloatinDiv img={thumbup} text1="Best Design" text2="Award" /></button>
-        </motion.div> */}
-
-        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
-        <div
-          className="blur"
-          style={{
-            background: "#C1F5FF",
-            top: "17rem",
-            width: "21rem",
-            height: "11rem",
-            left: "-9rem",
-          }}
-        ></div>
-      </div>
-    </div>
-      </div>
   )
 }
 
 export default Patient
-
